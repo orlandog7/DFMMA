6a94454ec4f3e283e6d43573b040db9b16ada053
orlandog7 <orlandog7@gmail.com>
2025-05-19 10:39:10 -0400

    Updated loader.js to fix paths for header and footer

--- blog/loader.js
+++ blog/loader.js
@@ -1,17 +1,17 @@
-// loader.js
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/DFMMA/header.html")
-      .then(res => res.text())
-      .then(data => {
-        const header = document.getElementById("header-placeholder");
-        if (header) header.innerHTML = data;
-      });
-  
-    fetch("/DFMMA/footer.html")
-      .then(res => res.text())
-      .then(data => {
-        const footer = document.getElementById("footer-placeholder");
-        if (footer) footer.innerHTML = data;
-      });
-  });
-  
+  // Fetch header
+  fetch("https://orlandog7.github.io/DFMMA/header.html")
+    .then(res => res.text())
+    .then(data => {
+      const header = document.getElementById("header-placeholder");
+      if (header) header.innerHTML = data;
+    });
+
+  // Fetch footer
+  fetch("https://orlandog7.github.io/DFMMA/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      const footer = document.getElementById("footer-placeholder");
+      if (footer) footer.innerHTML = data;
+    });
+});
