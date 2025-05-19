c1b4dbe68cd10728b6fa64f01f2d719ce1261f31
orlandog7 <orlandog7@gmail.com>
2025-05-19 10:31:20 -0400

    Updated loader.js with correct /DFMMA path

--- blog/loader.js
+++ blog/loader.js
@@ -1,13 +1,13 @@
 // loader.js
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/header.html")
+    fetch("/DFMMA/header.html")
       .then(res => res.text())
       .then(data => {
         const header = document.getElementById("header-placeholder");
         if (header) header.innerHTML = data;
       });
   
-    fetch("/footer.html")
+    fetch("/DFMMA/footer.html")
       .then(res => res.text())
       .then(data => {
         const footer = document.getElementById("footer-placeholder");
